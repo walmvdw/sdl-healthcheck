1599aaad121c20b649b2826b96968e47e63dd7dc
Kunal Shetye <kunal.shetye@gmail.com>
2018-02-24 17:24:28 +0100

    fixed issue with hardcoded jarname updated gitignore and added start.ps1 to run jar on windows updated version number removed public folder updated build file added build config

---
+++ src/dist/bin/start.ps1
@@ -0,0 +1,11 @@
+﻿
+cd ../
+
+If (Test-Path './config/logback.xml' -PathType Leaf) {
+    Start-Process java -ArgumentList '-Dlogging.config=./config/logback.xml', '-jar', '${jarName}'
+}
+Else {
+    Start-Process java -ArgumentList '-jar', '${jarName}'
+}
+
+echo "Started Service Healthceck"
